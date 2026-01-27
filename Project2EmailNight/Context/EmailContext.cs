8d97fa7e54cb5f8c55e2c4695a000b91cce6e416
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-01-27 22:03:16 +0300

    Add project files.

---
+++ Project2EmailNight/Context/EmailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project2EmailNight.Entities;
+
+namespace Project2EmailNight.Context
+{
+    public class EmailContext:IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-5V6MJ7S;initial catalog=Project2EmailNightDb;integrated security=true;trust server certificate=true");
+        }
+    }
+}
---
+++ Project2EmailNight/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project2EmailNight.Dtos;
+using Project2EmailNight.Entities;
+
+namespace Project2EmailNight.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CreateUser(UserRegisterDto userRegisterDto)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+            };
+            await _userManager.CreateAsync(appUser,userRegisterDto.Password);
+            return RedirectToAction("UserList");
+        }
+    }
+}
